1c129e3bf31592dfaf7bf7ac451b31a5f4ec6cdb
Nick Schimek <schimek@gmail.com>
2023-07-06 21:44:50 -0500

    hello world

---
+++ core/log.go
@@ -0,0 +1,13 @@
+package core
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+var Log = initializeLogger()
+
+func initializeLogger() *logrus.Logger {
+	log := logrus.New()
+	log.SetFormatter(&logrus.TextFormatter{TimestampFormat: "2006-01-02 15:04:05", FullTimestamp: true})
+	return log
+}
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/nschimek/nice-fixture-service/core"
+)
+
+const (
+	host = "localhost"
+	port = 8080
+)
+
+func main() {
+	core.Log.Info("Starting server...")
+
+	router := gin.Default()
+
+	router.GET("/api/v1/test", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"hello": "world",
+		})
+	})
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf("%s:%d", host, port),
+		Handler: router,
+	}
+
+	// Graceful server shutdown
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+				core.Log.Fatalf("Failed to initialize server: %v\n", err)
+		}
+	}()
+
+	core.Log.Infof("Server up and listening at %s", srv.Addr)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	core.Log.Warnf("Shutting down server...")
+	if err := srv.Shutdown(ctx); err != nil {
+		core.Log.Fatalf("Server forced to shutdown due to error: %v", err)
+	}
+}

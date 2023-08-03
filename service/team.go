361257ed7d07f2c121d4cdf624d400edd3d20e11
Nick Schimek <schimek@gmail.com>
2023-08-02 20:52:05 -0500

    only populated children func

--- service/team.go
+++ service/team.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"github.com/nschimek/nice-fixture-service/core"
 	"github.com/nschimek/nice-fixture-service/model"
 	"github.com/nschimek/nice-fixture-service/model/rest_error"
 	"github.com/nschimek/nice-fixture-service/repository"
@@ -20,8 +21,13 @@ func NewTeam(repo repository.Team) *team {
 }
 
 func (s *team) GetByParams(params model.TeamParams) ([]model.Team, *rest_error.Error) {
-	r, err := s.repo.GetAllByLeagueSeason(&model.TeamLeagueSeason{Season: params.Season, LeagueId: params.LeagueId})
-	return r, rest_error.NewInternal(err)
+	if r, err := s.repo.GetAllByLeagueSeason(&model.TeamLeagueSeason{Season: params.Season, LeagueId: params.LeagueId}); err == nil {
+		return core.OnlyPopulatedChildren[model.Team, model.TeamLeagueSeason](r, func(p model.Team) []model.TeamLeagueSeason {
+			return p.TeamLeagueSeasons
+		}), nil
+	} else {
+		return nil, rest_error.NewInternal(err)
+	}
 }
 
 func (s *team) GetById(id int) (*model.Team, *rest_error.Error) {
